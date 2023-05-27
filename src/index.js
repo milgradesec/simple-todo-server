cf77bd1391d6cde9b3c3a986916358e00e426273
milgradesec <38557680+milgradesec@users.noreply.github.com>
2023-05-27 12:55:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,23 @@ router.get('/', async c => {
 router.post("/register", async c => {
   const { username, password } = await c.req.json();
 
-  const count = await c.env.DB.prepare("SELECT COUNT(*) as count FROM users WHERE username = ?").bind(username).first();
+  const count = await c.env.DB.prepare("SELECT COUNT(*) as count FROM users WHERE username = ?")
+    .bind(username)
+    .first();
   if (count.count > 0) {
     return c.text("User already exists", { status: 403 });
   }
 
-  const info = await c.env.DB.prepare(`INSERT INTO users (username, password) VALUES (?1,?2)`).bind(username, password).run();
+  const info = await c.env.DB.prepare(`INSERT INTO users (username, password) VALUES (?1,?2)`)
+    .bind(username, password)
+    .run();
   if (!info.success) {
     return c.text("Internal Server Error", { status: 500 });
   }
 
-  const values = await c.env.DB.prepare(`SELECT id FROM users WHERE username = ? LIMIT 1`).bind(username).first();
+  const values = await c.env.DB.prepare(`SELECT id FROM users WHERE username = ? LIMIT 1`)
+    .bind(username)
+    .first();
   const response = { message: "OK", userId: values.id, token: "w9Z8RLiftZztnd2ygnt5SRHpcaahL3zPBFLS7MTJYb" };
   return c.json(response)
 })
@@ -30,7 +36,9 @@ router.post("/login", async c => {
   // }
 
   const { username, password } = await c.req.json();
-  const values = await c.env.DB.prepare(`SELECT id,password FROM users WHERE username = ? LIMIT 1`).bind(username).first();
+  const values = await c.env.DB.prepare(`SELECT id,password FROM users WHERE username = ? LIMIT 1`)
+    .bind(username)
+    .first();
   if (values.password == password) {
     const response = { message: "OK", userId: values.id, token: "w9Z8RLiftZztnd2ygnt5SRHpcaahL3zPBFLS7MTJYb" };
     return c.json(response)
