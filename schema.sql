7dd9df337f4fbe8f6bbe7a87ca5a0d65604bcc08
milgradesec <38557680+milgradesec@users.noreply.github.com>
2023-05-28 19:00:26 +0200

    Implementar crear y borrar listas

--- schema.sql
+++ schema.sql
@@ -1,7 +1,17 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS lists;
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+CREATE TABLE IF NOT EXISTS lists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 INSERT INTO users (username, password)
 VALUES ('admin@paesa.es', 'YTmFbcy94V3CFsGM');
+INSERT INTO lists(name, user_id)
+VALUES ('Compra', 1);
--- src/index.js
+++ src/index.js
@@ -1,72 +1,92 @@
 import { Hono } from 'hono'
 
-const router = new Hono()
+const app = new Hono()
 
-router.get('/', async c => {
+app.get('/', async c => {
   return c.text('Serverless Todo API v1', { status: 200 });
 });
 
-router.post("/register", async c => {
+app.post("/register", async c => {
   const { username, password } = await c.req.json();
 
-  const count = await c.env.DB.prepare("SELECT COUNT(*) as count FROM users WHERE username = ?")
+  const count = await c.env.DB.prepare(
+    `SELECT COUNT(*) AS count FROM users WHERE username = ?`
+  )
     .bind(username)
     .first();
   if (count.count > 0) {
     return c.text("User already exists", { status: 403 });
   }
 
-  const info = await c.env.DB.prepare(`INSERT INTO users (username, password) VALUES (?1,?2)`)
+  const info = await c.env.DB.prepare(
+    `INSERT INTO users (username, password) VALUES (?1,?2)`
+  )
     .bind(username, password)
     .run();
   if (!info.success) {
     return c.text("Internal Server Error", { status: 500 });
   }
 
-  const values = await c.env.DB.prepare(`SELECT id FROM users WHERE username = ? LIMIT 1`)
+  const values = await c.env.DB.prepare(
+    `SELECT id FROM users WHERE username = ? LIMIT 1`
+  )
     .bind(username)
     .first();
   const response = { message: "OK", userId: values.id, token: "w9Z8RLiftZztnd2ygnt5SRHpcaahL3zPBFLS7MTJYb" };
   return c.json(response)
 })
 
-router.post("/login", async c => {
-  // if (request.headers.get("Content-Type") != "application/json") {
-  //   return new Response('Bad Request', { status: 400 })
-  // }
-
+app.post("/login", async c => {
   const { username, password } = await c.req.json();
-  const values = await c.env.DB.prepare(`SELECT id,password FROM users WHERE username = ? LIMIT 1`)
+
+  const values = await c.env.DB.prepare(
+    `SELECT id,password FROM users WHERE username = ? LIMIT 1`
+  )
     .bind(username)
     .first();
   if (values.password == password) {
     const response = { message: "OK", userId: values.id, token: "w9Z8RLiftZztnd2ygnt5SRHpcaahL3zPBFLS7MTJYb" };
     return c.json(response)
   }
-
   return c.text("Invalid Authentication", { status: 401 });
 })
 
-router.post('/lists', async request => {
-  const responseBody = { message: "OK", listId: 23 }
+app.post('/lists', async c => {
+  const { name } = await c.req.json();
 
-  return new Response(JSON.stringify(responseBody), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
+  const info = await c.env.DB.prepare(`INSERT INTO lists (name) VALUES(?)`)
+    .bind(name)
+    .run();
+  if (!info.success) {
+    return new Response('ERROR', { status: 500 });
+  }
+
+  const values = await c.env.DB.prepare(
+    `SELECT id FROM lists WHERE name = ? LIMIT 1`
+  )
+    .bind(name)
+    .first();
+  return c.json({ message: "OK", listId: values.id });
+
 })
 
-router.put('/lists/:id', async request => {
+app.put('/lists/:id', async c => {
   return new Response('OK', { status: 200 });
 })
 
-router.delete('/lists/:id', async request => {
-  return new Response('OK', { status: 200 });
+app.delete('/lists/:id', async c => {
+  const { id } = c.req.param();
+
+  const info = await c.env.DB.prepare(`DELETE FROM lists WHERE id = ?`)
+    .bind(id)
+    .run();
+  if (info.success) {
+    return new Response('OK', { status: 200 });
+  }
+  return new Response('ERROR', { status: 500 });
 })
 
-router.post('/lists/:id/items', async request => {
+app.post('/lists/:id/items', async c => {
   const responseBody = { message: "OK", itemId: 14 }
 
   return new Response(JSON.stringify(responseBody), {
@@ -77,10 +97,10 @@ router.post('/lists/:id/items', async request => {
   })
 })
 
-router.put('/lists/:id/items/:itemId', async request => {
+app.put('/lists/:id/items/:itemId', async c => {
   return new Response('OK', { status: 200 });
 })
 
-router.all('*', () => new Response('Unauthorized ', { status: 401 }))
+app.all('*', () => new Response('Unauthorized', { status: 401 }))
 
-export default router
+export default app
